b1bf65f9866b3b53df00048f6e96ddd50376e6f5
nim-mahatani <nim.mahatani@gmail.com>
2017-11-17 12:25:28 +0530

    Update handlers.js

--- handlers.js
+++ handlers.js
@@ -63,4 +63,55 @@ exports.Changes = (slots, session, response) => {
             console.error(err);
             response.say("Oops. Something went wrong");
         });
+
+exports.NewCase = (slots, session, response) => {
+    let speechOutput = "OK, let's create a new case., What is the case subject?";
+    response.say(speechOutput);
+  };
+
+    
+exports.NewCaseSubject = (slots, session, response) => {
+    let speechOutput = "Got it. the subject is, " + slots.subject.value + "., What is the Description?";
+    session.attributes.subject = slots.subject.value;
+    response.say(speechOutput);
+  };
+
+exports.NewCaseDescription = (slots, session, response) => {
+    let speechOutput = "Got it. the description is, " + slots.description.value;
+    session.attributes.description = slots.description.value;
+    response.say(speechOutput);
+    salesforce.createNewCase({subject: session.attributes.subject, description: session.attributes.description, customername: 'Tom Ripley', priority: 'High', reason: 'Maintenance'})
+            .then(properties => {
+                if (properties && properties.length>0) {
+                    let text = `OK, Case has been created ${properties[0].get("Name")}`;
+                    properties.forEach(property => {
+                        text += `${property.get("Address__c")}, ${property.get("City__c")}: $${property.get("Price__c")}. <break time="0.5s" /> `;
+                    });
+                    response.say(text);
+                } else {
+                    response.say(`Sorry, Case was not created`);
+                }
+            })
+            .catch((err) => {
+                console.error(err);
+                response.say("Oops. Something went wrong");
+            });
+  };    
+    
+exports.NewCaseFinal = (slots, session, response) => {
+    salesforce.findPriceChanges()
+        .then(priceChanges => {
+            let text = "OK, here are the recent price changes: ";
+            priceChanges.forEach(priceChange => {
+                    let property = priceChange.get("Parent");
+                    text += `${property.Address__c}, ${property.City__c}.<break time="0.2s"/>
+                            Price changed from $${priceChange.get("OldValue")} to $${priceChange.get("NewValue")}.<break time="0.5s"/>`;
+            });
+           response.say(text);
+        })
+        .catch((err) => {
+            console.error(err);
+            response.say("Oops. Something went wrong");
+        });    
+    
 };
